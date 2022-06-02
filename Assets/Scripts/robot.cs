46990954b464e1e8985700e925484a1c596e9b3b
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2022-06-02 11:29:17 +0300

    to Ruth

--- Assets/Scripts/robot.cs
+++ Assets/Scripts/robot.cs
@@ -8,6 +8,11 @@ public class robot : MonoBehaviour
 	
 	private float HorizontalMovement = 0f;
 	private float VerticalMovement = 0f;
+	private float HorizontalMouseMovement = 0f;
+	
+	private float VerticalSpeed = 1f;
+	
+	public Animator MyAnimator;
 	
     // Start is called before the first frame update
     void Start()
@@ -18,12 +23,34 @@ public class robot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		
+		//moving commands
 		HorizontalMovement = Input.GetAxis("Horizontal");
-		VerticalMovement = Input.GetAxis("Vertical");
-		
+		VerticalMovement = Input.GetAxis("Vertical") * VerticalSpeed;
 		Vector3 speed = new Vector3(HorizontalMovement, 0, VerticalMovement);
 		
+		//turning commands
+		HorizontalMouseMovement += Input.GetAxis("Mouse X") * 3;
+		transform.localRotation = Quaternion.Euler(0, HorizontalMouseMovement, 0);
+		speed = transform.rotation * speed;
+		
+		if(VerticalMovement > 0){
+			MyAnimator.SetBool("WalkF", true);
+			
+			if (Input.GetKeyDown("left shift"))
+            {
+                MyAnimator.SetBool("RunF", true);
+                VerticalSpeed = 5f;
+                MyAnimator.SetBool("WalkF", false);
+            }
+		}
+		else{
+			MyAnimator.SetBool("WalkF", false);
+			MyAnimator.SetBool("RunF", false);
+            VerticalSpeed = 1f;
+		}
+		
+		
+		//final movement command
 		controller.Move(speed * Time.deltaTime);
         
     }
